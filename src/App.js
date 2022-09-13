d5daa25c1aa22f4eb4d623d5d8361f93b33eff20
Newton <altontonnalumasa@gmail.com>
2022-09-13 23:56:18 +0300

    Math magician UI section

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
-import logo from './logo.svg';
+// eslint-disable-next-line react/prefer-stateless-function
 import './App.css';
+import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learning React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = () => (
+  <Calculator />
+);
 export default App;
---
+++ src/components/Calculator.js
@@ -0,0 +1,47 @@
+// eslint-disable-line
+import React, { Component } from 'react';
+import '../css/style.css';
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { message: 'AC' };
+  }
+
+  createNumbers = () => {
+    const digits = [];
+    for (let i = 1; i < 10; i += 1) {
+      digits.push(
+        <button className="btn-grid" type="button" key={i}>{i}</button>,
+      );
+    }
+    return digits;
+  }
+
+  render() {
+    const { message } = this.state;
+    return (
+      <div className="App">
+        <div className="calculator">
+          <div className="display">
+            <span>0</span>
+          </div>
+          <div className="operators">
+            <button className="item" type="button">{ message }</button>
+            <button className="item" type="button">+/-</button>
+            <button className="item" type="button">%</button>
+            { this.createNumbers() }
+            <button className="item-1" type="button">/</button>
+            <button className="item-2" type="button">*</button>
+            <button className="item-3" type="button">-</button>
+            <button className="item-4" type="button">+</button>
+            <button className="item-5" type="button">=</button>
+            <button className="item-6" type="button">0</button>
+            <button className="item-7" type="button">.</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Calculator;
